2b022ffa09977470ccb22c759f61c33cb70909d2
Kiran Nayak <niekiran@gmail.com>
2019-10-02 16:01:25 +0600

    Itm send data

---
+++ All_source_codes/target/itm_send_data.c
@@ -0,0 +1,31 @@
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//					Implementation of printf like feature using ARM Cortex M3/M4/M7 ITM functionality
+//					This function will not work for ARM Cortex M0/M0+
+//					If you are using Cortex M0, then you can use semihosting feature of openOCD
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Debug Exception and Monitor Control Register base address
+#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
+
+/* ITM register addresses */
+#define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
+#define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
+
+void ITM_SendChar(uint8_t ch)
+{
+
+	//Enable TRCENA
+	DEMCR |= ( 1 << 24);
+
+	//enable stimulus port 0
+	ITM_TRACE_EN |= ( 1 << 0);
+
+	// read FIFO status in bit [0]:
+	while(!(ITM_STIMULUS_PORT0 & 1));
+
+	//Write to ITM stimulus port0
+	ITM_STIMULUS_PORT0 = ch;
+}
+
